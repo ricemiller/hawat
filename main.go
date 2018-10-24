69b1560a4a7bab5d0569b92afe057755888c318e
unknown <juanjose.mata@protonmail.com>
2018-10-24 14:30:43 +0100

    formatting

--- main.go
+++ main.go
@@ -1,18 +1,18 @@
 package main
 
 import (
-    "fmt"
-    //"time"
-    "os"
-    "bufio"
-    "strings"
-    "./board"
+	"fmt"
+	//"time"
+	"bufio"
+	"hawat/board"
+	"os"
+	"strings"
 )
 
 var ALLOWED_COMMANDS = [...]string{"uci", "debug", "isready", "setoption", "register", "ucinewgame", "position", "go", "stop", "ponderhit", "quit", "print", "perft"}
+
 const ENGINE_NAME = "Hawat"
 const ENGINE_AUTHOR = "ricemiller"
-
 
 /*
 
@@ -30,113 +30,110 @@ func background(quit chan struct{}) {
 */
 
 func UCIcommand(r *bufio.Reader) []string {
-    cmd, _ := r.ReadString('\n')
-    cmd = strings.TrimSpace(cmd)
-    return strings.Fields(cmd)
+	cmd, _ := r.ReadString('\n')
+	cmd = strings.TrimSpace(cmd)
+	return strings.Fields(cmd)
 }
 
-func IsGoodCMD (cmd string) bool {
-    for _, allowedCMD := range ALLOWED_COMMANDS {
-        if allowedCMD == cmd {
-            return true
-        }
-    }
-    return false
+func IsGoodCMD(cmd string) bool {
+	for _, allowedCMD := range ALLOWED_COMMANDS {
+		if allowedCMD == cmd {
+			return true
+		}
+	}
+	return false
 }
 
-func ClearCMDGarbage(cmd []string) []string{
-    if len(cmd) == 0 {
-        return cmd
-    }
+func ClearCMDGarbage(cmd []string) []string {
+	if len(cmd) == 0 {
+		return cmd
+	}
 
-    for !IsGoodCMD(cmd[0]) {
-        cmd = cmd[1:]
-        if len(cmd) == 0 {
-            return cmd
-        }
-    }
-    return cmd
+	for !IsGoodCMD(cmd[0]) {
+		cmd = cmd[1:]
+		if len(cmd) == 0 {
+			return cmd
+		}
+	}
+	return cmd
 }
 
 func main() {
-    var b board.Board
-    r := bufio.NewReader(os.Stdin)
+	var b board.Board
+	r := bufio.NewReader(os.Stdin)
 
-    //quit := make(chan struct{})
+	//quit := make(chan struct{})
 
+	// go background(quit) //start processing
 
-   // go background(quit) //start processing
+	// defer close(quit)
 
-    // defer close(quit)
+	for {
+		cmd := UCIcommand(r)
+		cmd = ClearCMDGarbage(cmd)
+		if len(cmd) > 0 {
 
-    for {
-        cmd := UCIcommand(r)
-        cmd = ClearCMDGarbage(cmd)
-        if len(cmd) > 0 {
+			switch cmd[0] {
+			case "uci":
+				fmt.Printf("id name %s\n", ENGINE_NAME)
+				fmt.Printf("id author %s\n", ENGINE_AUTHOR)
+				// SEND OPTIONS
+				fmt.Printf("uciok\n")
 
-            switch cmd[0]{
-            case "uci":
-                fmt.Printf("id name %s\n", ENGINE_NAME)
-                fmt.Printf("id author %s\n", ENGINE_AUTHOR)
-                // SEND OPTIONS
-                fmt.Printf("uciok\n")
+			case "debug":
 
-            case "debug":
+			case "isready":
+				fmt.Printf("readyok\n")
 
-            case "isready":
-                fmt.Printf("readyok\n")
+			case "setoption":
+				// RECIEVE OPTIONS
 
-            case "setoption":
-                // RECIEVE OPTIONS
+			case "register":
+				fmt.Printf("This is FOSS, baby\n")
 
-            case "register":
-                fmt.Printf("This is FOSS, baby\n")
+			case "ucinewgame":
+				b.Init()
 
+			case "position":
+				cmd = cmd[1:]
+				if len(cmd) > 0 {
+					switch cmd[0] {
+					case "fen":
+						cmd = cmd[1:]
+						if len(cmd) > 5 {
+							b.SetFEN(cmd[0:6])
+							cmd = cmd[6:]
+						}
 
-            case "ucinewgame":
-                b.Init()
+					case "startpos":
+						b.Init()
+						cmd = cmd[1:]
+					}
 
+					if len(cmd) > 0 {
+						if cmd[0] == "moves" {
+							cmd = cmd[1:]
+							b.Moves(cmd)
+						}
+					}
+				}
 
+			case "go":
 
-            case "position":
-                cmd = cmd[1:]
-                if len(cmd) > 0 {
-                    switch cmd[0] {
-                    case "fen":
-                        cmd = cmd[1:]
-                        if len(cmd) > 5 {
-                            b.SetFEN(cmd[0:6])
-                            cmd = cmd[6:]
-                        }
+			case "stop":
 
-                    case "startpos":
-                        b.Init()
-                        cmd = cmd[1:]
-                    }
+			case "ponderhit":
 
-                    if len(cmd) > 0 {
-                        if cmd[0] == "moves" {
-                            cmd = cmd[1:]
-                            b.Moves(cmd)
-                        }
-                    }
-                }
+			case "print":
+				b.Print()
 
-            case "go":
+			case "perft":
+                                b.Perft()
 
-            case "stop":
-
-            case "ponderhit":
-
-            case "print":
-                b.Print()
-
-            case "perft":
-
-            case "quit":
-                os.Exit(0)
-            default:
-            }
-        }
-    }
+			case "quit":
+				os.Exit(0)
+			default:
+			}
+		}
+	}
 }
